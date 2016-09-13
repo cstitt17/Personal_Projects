9e915c31aaec3ece9611649e13b2580780f654b0
cstit <cstit@192.168.2.4>
2016-09-13 17:51:58 -0400

    Draw Test

---
+++ Personal_Projects/src/drawtester/DrawComponent.java
@@ -0,0 +1,26 @@
+package drawtester;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
+import javax.swing.JComponent;
+@SuppressWarnings("serial")
+
+/**
+ * A class to test drawing things.
+ * @author cstit
+ */
+public class DrawComponent extends JComponent {
+	/**
+	 * A method to draw things.
+	 * @param g A Graphics object
+	 */
+	public void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		Rectangle box = new Rectangle(5,10,20,30);
+		Ellipse2D.Double ell = new Ellipse2D.Double(5,10,20,30);
+		Ellipse2D.Double cir = new Ellipse2D.Double(5,10,20,30);
+		g2d.draw(box);
+		g2d.draw(ell);
+		
+	}
+}
