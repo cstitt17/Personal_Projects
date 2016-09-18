8f874c53463b1a0a78761552bdc0ca71c91d322a
cstit <cstit@DESKTOP-KPFT18U>
2016-09-18 08:01:53 -0400

    Finish drawing test

--- Personal_Projects/src/drawtester/DrawComponent.java
+++ Personal_Projects/src/drawtester/DrawComponent.java
@@ -1,6 +1,6 @@
 package drawtester;
 import java.awt.*;
-import java.awt.geom.Ellipse2D;
+import java.awt.geom.*;
 
 import javax.swing.JComponent;
 @SuppressWarnings("serial")
@@ -19,8 +19,18 @@ public class DrawComponent extends JComponent {
 		Rectangle box = new Rectangle(5,10,20,30);
 		Ellipse2D.Double ell = new Ellipse2D.Double(5,10,20,30);
 		Ellipse2D.Double cir = new Ellipse2D.Double(5,10,20,20);
+		Line2D.Double seg = new Line2D.Double(15,20,15,40);
+		Point2D.Double from = new Point2D.Double(5, 20);
+		Point2D.Double to = new Point2D.Double(25, 20);
+		Line2D.Double segment = new Line2D.Double(from, to);
 		g2d.draw(box);
 		g2d.draw(ell);
 		g2d.draw(cir);
+		g2d.setColor(Color.GREEN);
+		g2d.fill(cir);
+		g2d.setColor(Color.RED);
+		g2d.draw(seg);
+		g2d.draw(segment);
+		g2d.drawString("Message", 25, 25);
 	}
 }
