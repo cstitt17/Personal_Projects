3b32e8a04a6d1453c0b149f8ba394e833037d4b6
cstit <cstit@10.10.125.16>
2016-11-15 10:46:52 -0500

    Begin fixing word game

---
+++ Personal_Projects/src/wordgame/WordGame2.java
@@ -0,0 +1,43 @@
+package wordgame;
+import java.io.File;
+import java.util.ArrayList;
+public class WordGame2 {
+
+	public static void main(String[] args) {
+		File WL = new File("C:\\Users\\cstit\\Desktop\\WordList.txt");
+        if (!(WL.exists())) {
+            System.out.println("This file does not exist.");
+        } else {
+        	System.out.println("The file does exist!");
+        	String usedPre;
+        	ArrayList<String> aList = new ArrayList<String>();
+        	ArrayList<String> bList = new ArrayList<String>();
+        	ArrayList<String> cList = new ArrayList<String>();
+        	ArrayList<String> dList = new ArrayList<String>();
+        	ArrayList<String> eList = new ArrayList<String>();
+        	ArrayList<String> fList = new ArrayList<String>();
+        	ArrayList<String> gList = new ArrayList<String>();
+        	ArrayList<String> hList = new ArrayList<String>();
+        	ArrayList<String> iList = new ArrayList<String>();
+        	ArrayList<String> jList = new ArrayList<String>();
+        	ArrayList<String> kList = new ArrayList<String>();
+        	ArrayList<String> lList = new ArrayList<String>();
+        	ArrayList<String> mList = new ArrayList<String>();
+        	ArrayList<String> nList = new ArrayList<String>();
+        	ArrayList<String> oList = new ArrayList<String>();
+        	ArrayList<String> pList = new ArrayList<String>();
+        	ArrayList<String> qList = new ArrayList<String>();
+        	ArrayList<String> rList = new ArrayList<String>();
+        	ArrayList<String> sList = new ArrayList<String>();
+        	ArrayList<String> tList = new ArrayList<String>();
+        	ArrayList<String> uList = new ArrayList<String>();
+        	ArrayList<String> vList = new ArrayList<String>();
+        	ArrayList<String> wList = new ArrayList<String>();
+        	ArrayList<String> xList = new ArrayList<String>();
+        	ArrayList<String> yList = new ArrayList<String>();
+        	ArrayList<String> zList = new ArrayList<String>();
+        	
+        }
+	}
+
+}
