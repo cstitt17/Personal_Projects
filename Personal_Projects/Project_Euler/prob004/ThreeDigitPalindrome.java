4919caf770ca12c8805f792d0a8e7a9c9a07f33a
cstit <cstit@192.168.1.7>
2017-04-01 17:02:28 -0400

    Continue Project Euler

--- Personal_Projects/Project_Euler/prob004/ThreeDigitPalindrome.java
+++ Personal_Projects/Project_Euler/prob004/ThreeDigitPalindrome.java
@@ -1,8 +1,12 @@
 package prob004;
 
+import java.util.ArrayList;
+
 public class ThreeDigitPalindrome {
 
 	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
 		for (int x = 100; x < 1000; x++) {
 			for (int y=100; y<1000; y++) {
 				int z = x*y;
@@ -11,9 +15,12 @@ public class ThreeDigitPalindrome {
 					if (zTest.charAt(c) != zTest.charAt(zTest.length()-c-1))
 						continue;
 					else
-						System.out.println(z);
+						list.add(z);
 			}
 		}
+		
+		list.trimToSize();
+		list
 	}
 
 }
