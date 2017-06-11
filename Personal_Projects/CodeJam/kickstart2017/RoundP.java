18fbd148f221026296171c171c731cce2a613641
cstit <cstit@192.168.1.8>
2017-06-11 14:05:22 -0400

    Practice reading and writing

--- Personal_Projects/CodeJam/kickstart2017/RoundP.java
+++ Personal_Projects/CodeJam/kickstart2017/RoundP.java
@@ -1,5 +1,15 @@
 package kickstart2017;
 
+import java.util.*;
+import java.io.*;
+
 public class RoundP {
-	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+		
+		while (in.hasNextLine())
+			System.out.println(in.nextLine());
+		
+		in.close();
+	}
 }
